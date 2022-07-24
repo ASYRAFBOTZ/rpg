def37b5e4025488bfac9cded7069f4ad30ef72bb
ASYRAFBOTZ <102161935+ASYRAFBOTZ@users.noreply.github.com>
2022-07-24 12:16:50 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,20 @@
 let handler = async (m, { conn, usedPrefix }) => {
 conn.sendButton(m.chat, `
 ╭─「 *DONASI* 」
+│ DANA [085646606905]
+│ GOPAY [08194694535]
+│ PULSA [085646606905]
+│ PULSA2 [081946945315]
+╰───
+
+╭─「 *NOTE* 」
 │ > Ingin donasi ke BOT?
 │ _Silahkan menghubungi owner_
-│ _https://wa.me/6285156299020_
+│ _https://wa.me/6281946945315
 │ _Hasil donasi akan dipakai untuk membuat BOT ONLINE 24 JAM_
 ╰────
 ╭─「 *NOTE* 」
-│ > _Tidak ada paksaan untuk kalian berdonasi~_
+│ > _Ada paksaan untuk kalian berdonasi_
 ╰────
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m)
 }
