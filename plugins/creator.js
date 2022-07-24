1c54cf72e4c524553548a773bf5b54455730fee5
ASYRAFBOTZ <102161935+ASYRAFBOTZ@users.noreply.github.com>
2022-07-24 12:13:22 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,13 +5,13 @@ let handler = async(m, { conn }) => {
 	ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
 	fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
 	let kontak2 = [
-        ['6285156299020', 'Bal', 'Owner ʙᴏᴛ', 'bal@balhis.codes', true]
+        ['6281946945315', 'ASYRAF', 'OWNER BOTZ', 'wekasenpai@gmail.com', true]
         ]
 	conn.p[id] = [
 	await conn.sendKontak(m.chat, kontak2, fkontak, { contexInfo: { forwardingScore: 99999, isForwarded: true } })
 	]
 	await delay(100)
-  return conn.sendMessage(m.chat, { text: `Hay kak @${await m.sender.split('@')[0]}, itu nomor ownerku jangan dispam yah ^_^`, mentions: [m.sender] }, { quoted: conn.p[id][0] })
+  return conn.sendMessage(m.chat, { text: `Hay Dek, Itu Owner Terganteng,Terramah, Terbaik, Terganz😎,TerPinter😎 Didunia Gw😎,Spam & Call?block`, mentions: [m.sender] }, { quoted: conn.p[id][0] })
   await delay(100)
   return delete conn.p[id]
 }
